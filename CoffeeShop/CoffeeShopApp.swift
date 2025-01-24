8e8cfd9eeabdb2f4340b4a1a5e58250a090c3bcb
m1roxx <nugmanovilyas228@gmail.com>
2025-01-24 13:30:56 +0500

    Initial commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Ilyas Nugmanov on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeShopTests/CoffeeShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopTests.swift
+//  CoffeeShopTests
+//
+//  Created by Ilyas Nugmanov on 24.01.2025.
+//
+
+import Testing
+@testable import CoffeeShop
+
+struct CoffeeShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
